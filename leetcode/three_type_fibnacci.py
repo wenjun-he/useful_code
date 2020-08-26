3408032cfec43577d3b11fa4d4fd04db9f7ffdc4
heonedream <onedream1216@163.com>
2020-08-26 23:57:52 +0800

    add first project

---
+++ leetcode/three_type_fibnacci.py
@@ -0,0 +1,43 @@
+#!/usr/bin
+from cal_time import *
+
+
+def fibnacci(n):
+    if n == 0 or n == 1 :
+        return 1
+    return fibnacci(n-1) + fibnacci(n-2)
+
+def fibnacci2(n):
+    li = [1, 1]
+    for i in range(2, n+1):
+        li.append(li[-1] + li[-2])
+    return li[n]
+
+def fibnacci3(n):
+    a = 1
+    b = 1
+    c = 1
+    if n == 0 or n == 1:
+        return 1
+    else:
+        for i in range(2, n+1):
+            c = a + b
+            a = b
+            b = c
+        return c
+
+@cal_time
+def fib1(n):
+    print(fibnacci(n))
+
+@cal_time
+def fib2(n):
+    print(fibnacci(n))
+
+@cal_time
+def fib3(n):
+    print(fibnacci(n))
+
+fib1(35)
+fib2(35)
+fib3(35)
